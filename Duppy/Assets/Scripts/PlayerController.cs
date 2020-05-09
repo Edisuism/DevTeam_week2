67fc5dffa92b15f1c80b82a35c6ec153e6a3fdff
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-05-09 18:48:33 +1000

    shader,controller

---
+++ Duppy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    //public Animator animator;
+    public GameObject flash;
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(flash, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
