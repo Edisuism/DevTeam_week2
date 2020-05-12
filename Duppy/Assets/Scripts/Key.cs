d846147b58a8d16c7212baead66d5a7dae4d0111
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-05-12 17:23:12 +1000

    fade

--- Duppy/Assets/Scripts/Key.cs
+++ Duppy/Assets/Scripts/Key.cs
@@ -10,6 +10,7 @@ public class Key : MonoBehaviour
     public GameObject player;
     public GameObject panel;
     public KeyType keyType;
+    public Image[] keyImage;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -25,12 +26,18 @@ public class Key : MonoBehaviour
         {
             case KeyType.key1:
                 player.GetComponent<PlayerController>().key1 = true;
+                Instantiate(keyImage[0], panel.transform);
+                Destroy(gameObject);
                 break;
             case KeyType.key2:
                 player.GetComponent<PlayerController>().key2 = true;
+                Instantiate(keyImage[1], panel.transform);
+                Destroy(gameObject);
                 break;
             case KeyType.key3:
                 player.GetComponent<PlayerController>().key3 = true;
+                Instantiate(keyImage[2], panel.transform);
+                Destroy(gameObject);
                 break;
         }
     }
--- Duppy/Assets/Scripts/MainMenu.cs
+++ Duppy/Assets/Scripts/MainMenu.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transitionAnim;
     public void playGame()
     {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
 
     public void quitGame()
     {
