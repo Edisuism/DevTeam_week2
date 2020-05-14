8c924afa2378692c326228e6d40f097d59d27a30
Kyle Hammer <kylethomashammer@gmail.com>
2020-05-14 21:29:44 +1000

    Cobwebs placed and integrated with new assets
    Got the new features from the animation branch and combined it with the cobwebs. Cobwebs are now places in the scene

--- Duppy/Assets/Scripts/CobwebTiles.cs
+++ Duppy/Assets/Scripts/CobwebTiles.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class InteractableTiles : MonoBehaviour
+public class CobwebTiles : MonoBehaviour
 {
     private PlayerController playerController;
     
